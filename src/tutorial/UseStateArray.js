7e512af2a0812842fc52c6184060c8eb1a3090dd
avanikatyayan <avanikatyayan@gmail.com>
2021-08-11 10:02:37 +0530

    avengers

--- src/tutorial/UseStateArray.js
+++ src/tutorial/UseStateArray.js
@@ -1,11 +1,24 @@
 import React from 'react'
-import {data} from "./data";
+import { data } from "./data";
 function UseStateArray() {
-    const[people,setPeople] = React.useState({})
+    const[people,setPeople] = React.useState(data)
     return (
-        <div>
-            <h2>Use state array is in action</h2>
-        </div>
+        <>
+            {
+                people.map((person) =>
+                {
+                    const {id, name}= person;
+                    return(
+                        <div key ={id} className ='item'>
+                            <h4>{name}</h4>
+                        </div>
+                    )
+                }
+                )}
+                <button className="btn" onClick={() =>setPeople}>
+                    Clear the Avengers
+                </button>
+        </>
     )
 }
 
