cc75a65df2f0199a955e0f8d46b18666d42e714d
avanikatyayan <avanikatyayan@gmail.com>
2021-08-11 09:46:59 +0530

    b

---
+++ src/tutorial/UseStateArray.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {data} from "./data";
+function UseStateArray() {
+    const[people,setPeople] = React.useState({})
+    return (
+        <div>
+            <h2>Use state array is in action</h2>
+        </div>
+    )
+}
+
+export default UseStateArray
